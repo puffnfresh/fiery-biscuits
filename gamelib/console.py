c6df735331a5560c630d710766990647afd266a0
Brian McKenna <brian@brianmckenna.org>
2008-09-09 12:03:49 +0000

    Added in a debugging console (access it with `)

---
+++ gamelib/console.py
@@ -0,0 +1,91 @@
+#!/usr/bin/evn python
+
+import pyglet
+import sys
+from pyglet.window import key
+
+INPUT_INDICATOR = ">>> "
+
+class Console():
+    def __init__(self, window, globals):
+        self.active = False
+        self.globals = globals
+        self.history = []
+        self.history_steps = 0
+
+        self.document = pyglet.text.document.UnformattedDocument(INPUT_INDICATOR)
+        self.document.set_style(0, 0, {'color' : (255, 255, 255, 255)})
+        self.layout = pyglet.text.layout.IncrementalTextLayout(self.document, window.width, window.height, True)
+        self.caret = pyglet.text.caret.Caret(self.layout)
+        self.caret.color = (255, 255, 255)
+        self.caret.position = len(self.document.text)
+
+    def is_active(self):
+        return self.active
+
+    def toggle(self):
+        self.active = not self.active
+
+    def run_input(self):
+        command = self.get_command()
+        self.history.append(command)
+        try:
+            output = repr(eval(command, self.globals, {}))
+        except Exception, inst:
+            output = repr(inst)
+        self.document.text += "\n" + output + "\n" + INPUT_INDICATOR
+        self.caret.position = len(self.document.text)
+        self.history_steps = 0
+
+    def get_command(self):
+        return self.document.text.splitlines()[-1][len(INPUT_INDICATOR):]
+
+    def clear_command(self):
+        command = self.get_command()
+        if len(command) > 0:
+            self.document.text = self.document.text[:-len(command)]
+        self.reset_caret()
+
+    def reset_caret(self):
+        self.caret.position = len(self.document.text)
+
+    def at_start(self):
+        indicator_pos = self.document.text.rfind("\n" + INPUT_INDICATOR)
+        if self.caret.position <= indicator_pos + len(INPUT_INDICATOR) + 1 or len(self.document.text) == len(INPUT_INDICATOR):
+            return True
+        return False
+        
+    def on_text(self, text):
+        if text == "\r":
+            self.run_input()
+        elif text != "`":
+            self.caret.on_text(text)
+
+    def on_text_motion(self, motion):
+        if motion == key.MOTION_BACKSPACE or motion == key.MOTION_LEFT:
+            if self.at_start():
+                return
+            self.caret.on_text_motion(motion)    
+        elif motion == key.MOTION_RIGHT:
+            if self.caret.position >= len(self.document.text):
+                return
+            self.caret.on_text_motion(motion)
+        elif motion == key.MOTION_DELETE:
+            self.caret.on_text_motion(motion)
+        elif motion in [key.MOTION_UP, key.MOTION_PREVIOUS_PAGE, key.MOTION_DOWN, key.MOTION_NEXT_PAGE]:
+            if len(self.history) <= 0:
+                return
+            self.clear_command()
+
+            if motion == key.MOTION_UP or motion == key.MOTION_PREVIOUS_PAGE:
+                self.history_steps += 1
+            else:
+                self.history_steps -= 1
+
+            command = self.history[-self.history_steps % len(self.history)]
+            self.document.insert_text(self.caret.position, command)
+            self.reset_caret()
+        elif motion == key.MOTION_END_OF_LINE:
+            self.reset_caret()
+        #elif motion == key.MOTION_BEGINNING_OF_LINE:
+        #    self.reset_caret()
--- gamelib/main.py
+++ gamelib/main.py
@@ -12,12 +12,17 @@ import math
 import pyglet
 import ode
 
+from console import Console
+
 # Gameplay constants
 DISPLAY_FPS = True
-GRAVITY = -100
+GRAVITY = -100.0
 PHYSICS_STEP = 0.005
 FPS_LIMIT = 0 # 0 for no limit
-PLAYER_SPEED = 100.0
+PLAYER_SPEED = 5.0
+PLAYER_SPEED_STEP = 1.0
+PLAYER_SPEED_TIME = 25.0 # The amount of times speed should be updated per second
+CONSOLE_SPEED = 0.25
 
 # Set up Pyglet
 window = pyglet.window.Window()
@@ -37,6 +42,8 @@ wall_right = ode.GeomPlane(space, (-1, 0, 0), -window.width)
 # Player
 class Player():
     def __init__(self):
+        self.speed_dt = 0
+
         self.image = pyglet.image.load(data.filepath('yarn.png'))
         self.image.anchor_x = self.image.width / 2
         self.image.anchor_y = self.image.height / 2
@@ -45,11 +52,11 @@ class Player():
 
         self.body = ode.Body(world)
         self.body.setPosition((200, 300, 0))
-        #self.body.setAngularVel((0, 0, 0))
+        self.body.setAngularVel((0, 0, 10))
         self.body.setLinearVel((500, 0, 0))
 
         self.mass = ode.Mass()
-        self.mass.setCylinder(1.0, 3, self.image.width / 2, 1)
+        self.mass.setCylinder(1, 3, self.image.width / 2, 1)
         self.body.setMass(self.mass)
 
         self.geom = ode.GeomCylinder(space, self.image.width / 2)
@@ -71,12 +78,31 @@ class Player():
         rotation = math.atan2(matrix[4], matrix[3]) * 180/math.pi - 90
         self.sprite.rotation = rotation
 
+    def move_left(self, dt):
+        self.update_speed(dt, 'left')
+
+    def move_right(self, dt):
+        self.update_speed(dt, 'right')
+
+    def update_speed(self, dt, direction):
+        zspeed = self.body.getAngularVel()[2]
+        if direction == 'left' and zspeed < PLAYER_SPEED:
+            zspeed = min(PLAYER_SPEED, zspeed + PLAYER_SPEED_STEP * PLAYER_SPEED_TIME * dt)
+        elif direction == 'right' and zspeed > -PLAYER_SPEED:
+            zspeed = max(-PLAYER_SPEED, zspeed - PLAYER_SPEED_STEP * PLAYER_SPEED_TIME * dt)
+        self.body.setAngularVel((0, 0, zspeed))
+
 player = Player()
+
+# Console
+console = Console(window, globals())
 
 # Physics
 def near_callback(args, geom1, geom2):
     contacts = ode.collide(geom1, geom2)
     for contact in contacts:
+        contact.setMu(ode.Infinity)
+        contact.setBounce(1)
         joint = ode.ContactJoint(world, contactgroup, contact)
         joint.attach(geom1.getBody(), geom2.getBody())
 
@@ -96,8 +122,39 @@ def update(dt):
 
 # Input
 @window.event
-def on_key_press(symbol, modifers):
+def on_text(text):
+    if console.is_active():
+		console.on_text(text)
+
+@window.event
+def on_text_motion(motion):
+    if console.is_active():
+		console.on_text_motion(motion)
+
+@window.event
+def on_key_press(symbol, modifiers):
     key = pyglet.window.key
+
+    # Block all input while console is open (except closing the console)
+    if symbol == key.GRAVE:
+        console.toggle()
+        return
+    elif console.is_active():
+        return
+
+    if symbol == key.LEFT:
+        pyglet.clock.schedule_interval(player.move_left, 1.0 / PLAYER_SPEED_TIME)
+    elif symbol == key.RIGHT:
+        pyglet.clock.schedule_interval(player.move_right, 1.0 / PLAYER_SPEED_TIME)
+
+@window.event
+def on_key_release(symbol, modifers):
+    key = pyglet.window.key
+
+    if symbol == key.LEFT:
+        pyglet.clock.unschedule(player.move_left)
+    elif symbol == key.RIGHT:
+        pyglet.clock.unschedule(player.move_right)
 
 # Graphics
 if DISPLAY_FPS:
@@ -107,6 +164,9 @@ if DISPLAY_FPS:
 def on_draw():
     window.clear()
     batch.draw()
+
+    if console.is_active():
+        console.layout.draw()
 
     if DISPLAY_FPS:
         fps_display.draw()
@@ -118,3 +178,6 @@ def main():
     pyglet.clock.set_fps_limit(FPS_LIMIT)
     pyglet.clock.schedule(update)
     pyglet.app.run()
+
+if __name__ == "__main__":
+    main()
